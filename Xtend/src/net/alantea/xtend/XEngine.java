14a7e1561c7d933622dc3b84bf17f306a3fcb859
Jean Rébillat <jean.rebillat@free.fr>
2016-12-09 15:48:48 +0100

    documentation

--- Xtend/src/net/alantea/xtend/XEngine.java
+++ Xtend/src/net/alantea/xtend/XEngine.java
@@ -9,67 +9,78 @@ import javax.script.ScriptException;
 /**
  * The Class XEngine is used to simplify access to Javascript commands.
  */
-public class XEngine {
-	
-	/** The nashorn engine. */
-	private ScriptEngine nashornEngine;
-	
-	/**
-	 * Instantiates a new engine.
-	 */
-	public XEngine()
-	{
-	    ScriptEngineManager scriptManager = new ScriptEngineManager();
-	    nashornEngine = scriptManager.getEngineByName("nashorn");
-	}
+public class XEngine
+{
 
-	/**
-	 * Evaluate a command line.
-	 *
-	 * @param cmd the command line
-	 * @return the returned object
-	 * @throws Xception the exception
-	 */
-	public Object eval(String cmd) throws Xception {
-		try {
-			return nashornEngine.eval(cmd);
-		} catch (ScriptException e) {
-			throw new Xception("error evaluating script", e);
-		}
-	}
+   /** The nashorn engine. */
+   private ScriptEngine nashornEngine;
 
-	/**
-	 * Evaluate a stream.
-	 *
-	 * @param stream the stream
-	 * @return the returned object
-	 * @throws Xception the exception
-	 */
-	public Object eval(Reader stream) throws Xception {
-		try {
-			return nashornEngine.eval(stream);
-		} catch (ScriptException e) {
-			throw new Xception("error evaluating script file", e);
-		}
-	}
+   /**
+    * Instantiates a new engine.
+    */
+   public XEngine()
+   {
+      ScriptEngineManager scriptManager = new ScriptEngineManager();
+      nashornEngine = scriptManager.getEngineByName("nashorn");
+   }
 
-	/**
-	 * Put a variable.
-	 *
-	 * @param name the variable name
-	 * @param object the content object
-	 */
-	public void put(String name, Object object) {
-		nashornEngine.put(name, object);
-	}
+   /**
+    * Evaluate a command line.
+    *
+    * @param cmd the command line
+    * @return the returned object
+    * @throws Xception the exception
+    */
+   public Object eval(String cmd) throws Xception
+   {
+      try
+      {
+         return nashornEngine.eval(cmd);
+      }
+      catch (ScriptException e)
+      {
+         throw new Xception("error evaluating script", e);
+      }
+   }
 
-	/**
-	 * Gets the variable.
-	 *
-	 * @param name the variable name
-	 * @return the returned object
-	 */
-	public Object get(String name) {
-		return nashornEngine.get(name);
-	}
+   /**
+    * Evaluate a stream.
+    *
+    * @param stream the stream
+    * @return the returned object
+    * @throws Xception the exception
+    */
+   public Object eval(Reader stream) throws Xception
+   {
+      try
+      {
+         return nashornEngine.eval(stream);
+      }
+      catch (ScriptException e)
+      {
+         throw new Xception("error evaluating script file", e);
+      }
+   }
+
+   /**
+    * Put a variable.
+    *
+    * @param name the variable name
+    * @param object the content object
+    */
+   public void put(String name, Object object)
+   {
+      nashornEngine.put(name, object);
+   }
+
+   /**
+    * Gets the variable.
+    *
+    * @param name the variable name
+    * @return the returned object
+    */
+   public Object get(String name)
+   {
+      return nashornEngine.get(name);
+   }
 }
